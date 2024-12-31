438068844468a7b7693390deafb9cda591517675
真学不会 <03211003@cumt.edu.cn>
2024-12-31 18:02:04 +0800

    Update train.py

--- train.py
+++ train.py
@@ -237,6 +237,9 @@ def training_report(tb_writer, iteration, Ll1, loss, l1_loss, elapsed, testing_i
                 for idx, viewpoint in enumerate(config['cameras']):
                     image = torch.clamp(renderFunc(viewpoint, scene.gaussians, *renderArgs)["render"], 0.0, 1.0)
                     gt_image = torch.clamp(viewpoint.original_image.to("cuda"), 0.0, 1.0)
+                    mask = viewpoint.mask.to("cuda").float()
+                    image = image * mask
+                    gt_image = gt_image * mask
                     if train_test_exp:
                         image = image[..., image.shape[-1] // 2:]
                         gt_image = gt_image[..., gt_image.shape[-1] // 2:]
