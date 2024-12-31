924f153007bf5b42dfd45bc1b4e473914d287fa5
真学不会 <03211003@cumt.edu.cn>
2024-12-31 16:28:20 +0800

    Update dataset_readers.py

--- scene/dataset_readers.py
+++ scene/dataset_readers.py
@@ -128,6 +128,7 @@ def fetchPly(path):
     plydata = PlyData.read(path)
     vertices = plydata['vertex']
     positions = np.vstack([vertices['x'], vertices['y'], vertices['z']]).T
+    print("positions.shape:",positions.shape)
     colors = np.vstack([vertices['red'], vertices['green'], vertices['blue']]).T / 255.0
     normals = np.vstack([vertices['nx'], vertices['ny'], vertices['nz']]).T
     return BasicPointCloud(points=positions, colors=colors, normals=normals)
