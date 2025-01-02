c2a906890abfcc34f2356ebf914afa3a4e8ceaea
真学不会 <03211003@cumt.edu.cn>
2025-01-02 10:27:39 +0800

    Update image_utils.py

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -17,10 +17,11 @@ def mse(img1, img2):
 def psnr(img1, img2,mask=None):
     if mask is not None:
         mse = (((img1 - img2)) ** 2) * mask
+        mse = mse.view(mse.shape[0], -1)
         num_valid_elements = mask.view(mask.shape[0], -1).sum(1)
-        num_valid_elements = torch.max(num_valid_elements, torch.tensor(1e-10).to(mask.device))  # 防止除以0
-        mse = mse.view(mse.shape[0], -1).sum(1) / num_valid_elements
+        num_valid_elements = torch.max(num_valid_elements, torch.tensor(1e-10).to(mask.device)) 
+        mse = mse.sum(1) / num_valid_elements
     else:
         mse = (((img1 - img2)) ** 2).view(img1.shape[0], -1).mean(1, keepdim=True)
-    
+
     return 20 * torch.log10(1.0 / torch.sqrt(mse))
