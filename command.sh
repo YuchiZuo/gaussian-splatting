033c7037687226a6120b656efc8d435d1c9b6f37
真学不会 <03211003@cumt.edu.cn>
2025-01-02 15:25:34 +0800

    1

--- command.sh
+++ command.sh
@@ -1,1 +1,3 @@
-python train.py -s ./data/R63_two_view/ --resolution 1 --iterations 10000 --test_iterations 100 1000 5000
+python train.py -s ./data/R63_two_view/ --resolution 1 --iterations 2000 --test_iterations 100 1000 --save_iterations 1 500
+
+python render.py -m ./output/5d9d18ce-a
