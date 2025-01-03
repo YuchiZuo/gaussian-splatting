d48a74fd5ecc5359bb590f1ad5db4db6341df5c3
真学不会 <03211003@cumt.edu.cn>
2025-01-03 18:11:44 +0800

    Update __init__.py

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -84,7 +84,8 @@ class OptimizationParams(ParamGroup):
         self.position_lr_max_steps = 30_000
         self.feature_lr = 0.0025
         self.opacity_lr = 0.025
-        self.scaling_lr = 0.005
+        # self.scaling_lr = 0.005
+        self.scaling_lr = 0
         self.rotation_lr = 0.001
         self.exposure_lr_init = 0.01
         self.exposure_lr_final = 0.001
