c27131ed7c8979df12a7b57d486597518c144488
真学不会 <03211003@cumt.edu.cn>
2025-01-03 17:47:24 +0800

    1

--- command.sh
+++ command.sh
@@ -1,3 +1,7 @@
 python train.py -s ./data/R63_two_view/ --resolution 1 --iterations 10000 --test_iterations 1000 5000 --save_iterations 1 5000
 
+
+python train.py -s ./data/R63_two_view/ --resolution 1 --iterations 1000 --test_iterations 1 100 500 --save_iterations 1 500
+
+
 python render.py -m ./output/5d9d18ce-a --iterations 1
